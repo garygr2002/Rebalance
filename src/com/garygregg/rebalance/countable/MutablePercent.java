2e880eb33ff619ee6db56f5d34fea8359c22de1e
Gary Gregg <garygr2002@yahoo.com>
2021-06-16 22:37:30 -0700

    Initial release of Percent and MutablePercent (largely copied from Shares and MutableShares).

---
+++ src/com/garygregg/rebalance/countable/MutablePercent.java
@@ -0,0 +1,195 @@
+package com.garygregg.rebalance.countable;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.text.NumberFormat;
+import java.util.Objects;
+
+public class MutablePercent extends MutableCountable
+        implements Factory<Percent> {
+
+    // A class comparable to this one
+    private static final Class<Percent> comparableClass = Percent.class;
+
+    // The precision of percent
+    private static final int precision = ICountable.getPercentPrecision();
+
+    // Our number formatter
+    private static final NumberFormat formatter =
+            ICountable.createFormat(precision);
+
+    // A container for immutable currency
+    private final Container<Percent> container = new Container<>(this);
+
+    /**
+     * Constructs percent.
+     *
+     * @param value    The value of the percent
+     * @param truncate True if the value should be truncated, false if rounded
+     */
+    @SuppressWarnings("unused")
+    MutablePercent(double value, boolean truncate) {
+        super(value, truncate);
+    }
+
+    /**
+     * Constructs percent with a default truncate flag.
+     *
+     * @param value The value of the percent
+     */
+    public MutablePercent(double value) {
+        super(value);
+    }
+
+    /**
+     * Constructs percent from other mutable percent.
+     *
+     * @param percent Other mutable percent
+     */
+    public MutablePercent(@NotNull MutablePercent percent) {
+        super(percent.getValue());
+    }
+
+    /**
+     * Constructs percent from immutable percent.
+     *
+     * @param percent Immutable percent
+     */
+    public MutablePercent(@NotNull Percent percent) {
+        super(percent.getValue());
+    }
+
+    /**
+     * Adds a percent value.
+     *
+     * @param percent Another percent value
+     * @return The value that was set
+     */
+    public double add(@NotNull Percent percent) {
+
+        // Add the value, and return the result.
+        set(getValue() + percent.getValue());
+        return getValue();
+    }
+
+    /**
+     * Adds a percent value.
+     *
+     * @param percent Another percent value
+     * @return The value that was set
+     */
+    public double add(@NotNull MutablePercent percent) {
+
+        // Add the value, and return the result.
+        set(getValue() + percent.getValue());
+        return getValue();
+    }
+
+    @Override
+    public boolean areNotEqual(double value) {
+        return !ICountable.areEqual(value, getValue(), getPrecision());
+    }
+
+    @Override
+    protected void clear() {
+
+        /*
+         * There is a null check here because initialization of the container
+         * might not have occurred when this method is called.
+         */
+        if (null != container) {
+            container.clear();
+        }
+    }
+
+    @Override
+    @SuppressWarnings("EqualsWhichDoesntCheckParameterClass")
+    public boolean equals(Object object) {
+        return ICountable.areEqual(this, object, this.getClass(),
+                comparableClass, getPrecision());
+    }
+
+    /**
+     * Gets an immutable equivalent of this object.
+     *
+     * @return An immutable equivalent of this object
+     */
+    public @NotNull Percent getImmutable() {
+        return container.get();
+    }
+
+    @Override
+    public int getPrecision() {
+        return precision;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getValue());
+    }
+
+    @Override
+    @SuppressWarnings("EqualsBetweenInconvertibleTypes")
+    public boolean isNotZero() {
+        return !equals(Percent.getZero());
+    }
+
+    /**
+     * Returns true if the percent is one hundred, false otherwise
+     *
+     * @return True if the percent is one hundred, false otherwise
+     */
+    @SuppressWarnings("EqualsBetweenInconvertibleTypes")
+    public boolean isOneHundred() {
+        return equals(Percent.getOneHundred());
+    }
+
+    @Override
+    public @NotNull Percent produce() {
+        return new Percent(this);
+    }
+
+    /**
+     * Sets a percent value.
+     *
+     * @param percent Another percent value
+     * @return The value that was set
+     */
+    public double set(@NotNull MutablePercent percent) {
+
+        // Set the value, and return the result.
+        set(percent.getValue());
+        return getValue();
+    }
+
+    /**
+     * Subtracts a percent value.
+     *
+     * @param percent Another percent value
+     * @return The value that was set
+     */
+    public double subtract(@NotNull Percent percent) {
+
+        // Subtract the value, and return the result.
+        set(getValue() - percent.getValue());
+        return getValue();
+    }
+
+    /**
+     * Subtracts a percent value.
+     *
+     * @param percent Another percent value
+     * @return The value that was set
+     */
+    public double subtract(@NotNull MutablePercent percent) {
+
+        // Subtract the value, and return the result.
+        set(getValue() - percent.getValue());
+        return getValue();
+    }
+
+    @Override
+    public String toString() {
+        return formatter.format(getValue());
+    }
+}
---
+++ src/com/garygregg/rebalance/countable/Percent.java
@@ -0,0 +1,144 @@
+package com.garygregg.rebalance.countable;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.text.NumberFormat;
+import java.util.Objects;
+
+public class Percent extends Countable {
+
+    // A class comparable to this one
+    private static final Class<MutablePercent> comparableClass =
+            MutablePercent.class;
+
+    // A known one hundred
+    private static final Percent oneHundred = new Percent(100.);
+
+    // The precision of percent
+    private static final int precision = ICountable.getPercentPrecision();
+
+    // A known minimum
+    private static final Percent minimum =
+            new Percent(1. / ICountable.calculateFactor(precision));
+
+    // Our number formatter
+    private static final NumberFormat formatter =
+            ICountable.createFormat(precision);
+
+    // A known zero
+    private static final Percent zero = new Percent(0.);
+
+    /**
+     * Constructs percent.
+     *
+     * @param value    The value of the percent
+     * @param truncate True if the value should be truncated, false if rounded
+     */
+    Percent(double value, boolean truncate) {
+        super(value, precision, truncate);
+    }
+
+    /**
+     * Constructs percent with a default truncate flag.
+     *
+     * @param value The value of the percent
+     */
+    public Percent(double value) {
+        super(value, precision);
+    }
+
+    /**
+     * Constructs percent from other percent.
+     *
+     * @param percent Other percent
+     */
+    Percent(@NotNull Percent percent) {
+        super(percent.getValue(), precision);
+    }
+
+    /**
+     * Constructs percent from mutable percent.
+     *
+     * @param percent Mutable percent
+     */
+    Percent(@NotNull MutablePercent percent) {
+        super(percent.getValue(), precision);
+    }
+
+    /**
+     * Gets a known minimum.
+     *
+     * @return A known minimum
+     */
+    public static Percent getMinimum() {
+        return minimum;
+    }
+
+    /**
+     * Gets a known one hundred.
+     *
+     * @return A known one hundred
+     */
+    public static Percent getOneHundred() {
+        return oneHundred;
+    }
+
+    /**
+     * Gets a known zero.
+     *
+     * @return A known zero
+     */
+    public static Percent getZero() {
+        return zero;
+    }
+
+    @Override
+    public boolean areNotEqual(double value) {
+        return !ICountable.areEqual(value, getValue(), getPrecision());
+    }
+
+    /**
+     * Returns true the percents are one hundred, false otherwise
+     *
+     * @return True if the percents are one hundred, false otherwise
+     */
+    public boolean areOneHundred() {
+        return equals(getOneHundred());
+    }
+
+    @Override
+    @SuppressWarnings("EqualsWhichDoesntCheckParameterClass")
+    public boolean equals(Object object) {
+        return ICountable.areEqual(this, object, this.getClass(),
+                comparableClass, getPrecision());
+    }
+
+    @Override
+    public int getPrecision() {
+        return precision;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getValue());
+    }
+
+    @Override
+    public boolean isNotZero() {
+        return !equals(getZero());
+    }
+
+    /**
+     * Returns true if the percent is one hundred, false otherwise
+     *
+     * @return True if the percent is one hundred, false otherwise
+     */
+    public boolean isOneHundred() {
+        return equals(getOneHundred());
+    }
+
+    @Override
+    public String toString() {
+        return formatter.format(getValue());
+    }
+}
