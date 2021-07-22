f889221667c05ebeeaf7263ac5f5cffc5278cb8e
Gary Gregg <garygr2002@yahoo.com>
2021-07-22 16:01:30 -0700

    Fill out details of the reset dispatcher.

--- src/com/garygregg/rebalance/conductor/Reset.java
+++ src/com/garygregg/rebalance/conductor/Reset.java
@@ -2,8 +2,11 @@ package com.garygregg.rebalance.conductor;
 
 import com.garygregg.rebalance.CommandLineId;
 import com.garygregg.rebalance.Informer;
+import com.garygregg.rebalance.PreferenceManager;
 import com.garygregg.rebalance.cla.CLAException;
 import com.garygregg.rebalance.cla.Dispatch;
+import com.garygregg.rebalance.cla.DoublePreferenceDispatch;
+import com.garygregg.rebalance.cla.LevelPreferenceDispatch;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.PrintStream;
@@ -19,10 +22,42 @@ class Reset extends Informer implements Dispatch<CommandLineId> {
         super(stream);
     }
 
+    /**
+     * Resets preferences.
+     */
+    private static void resetPreferences() {
+
+        // Get the preference manager, and reset the S&P 500 current level.
+        final PreferenceManager manager = PreferenceManager.getInstance();
+        manager.setCurrent(null);
+
+        // Reset the backup destination, and the S&P 500 high level.
+        manager.setDestination(null);
+        manager.setHigh(null);
+
+        /*
+         * Reset the expected annual inflation rate, the desired logging level,
+         * and the path for the data files.
+         */
+        manager.setInflation(null);
+        manager.setLevel(null);
+        manager.setPath(null);
+    }
+
     @Override
     public void dispatch(String argument) throws CLAException {
-        getStream().printf("Reset command received with argument of '%s'.",
-                argument);
+
+        /*
+         * Throw a new CLA exception of the argument is not null. The reset
+         * option does not take an argument.
+         */
+        if (null != argument) {
+            throw new CLAException(String.format("The reset option does not " +
+                    "take an argument; received %s.", argument));
+        }
+
+        // Null argument, as expected. Reset the preferences.
+        resetPreferences();
     }
 
     @Override
