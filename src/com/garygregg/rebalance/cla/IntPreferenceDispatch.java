0d8fc44641ad9fd9d2afc5ed9199c32b42a4bd3c
Gary Gregg <garygr2002@yahoo.com>
2021-07-22 02:00:43 -0700

    Inherit from the FlaggedPreferenceDispatch, and allow 'put' to throw a CLAException; throw the exception if the put value cannot be parsed as an integer, or if the value is negative and cannot be.

--- src/com/garygregg/rebalance/cla/IntPreferenceDispatch.java
+++ src/com/garygregg/rebalance/cla/IntPreferenceDispatch.java
@@ -6,27 +6,29 @@ import java.io.PrintStream;
 import java.util.prefs.Preferences;
 
 public class IntPreferenceDispatch<KeyType extends Enum<KeyType>>
-        extends PreferenceDispatch<KeyType> {
+        extends FlaggedPreferenceDispatch<KeyType> {
 
     // The default value to use in case of no current preference
     private final int defaultValue;
 
     /**
-     * Constructs the integer preferences dispatch.
+     * Constructs the integer preference dispatch.
      *
      * @param key          The key for this dispatch
      * @param preferences  The preferences object to use
      * @param stream       The output stream for messages
+     * @param flag         The flag; true if negatives are okay, false otherwise
      * @param defaultValue The default value to use in case of no current
      *                     preference
      */
     public IntPreferenceDispatch(@NotNull KeyType key,
                                  @NotNull Preferences preferences,
                                  @NotNull PrintStream stream,
+                                 boolean flag,
                                  int defaultValue) {
 
         // Call the superclass constructor, and set the default value.
-        super(key, preferences, stream);
+        super(key, preferences, stream, flag);
         this.defaultValue = defaultValue;
     }
 
@@ -46,7 +48,35 @@ public class IntPreferenceDispatch<KeyType extends Enum<KeyType>>
     }
 
     @Override
-    protected void put(@NotNull String value) {
-        getPreferences().putInt(getKeyName(), Integer.parseInt(value));
+    protected void put(@NotNull String value) throws CLAException {
+
+        // Declare a variable to receive a parsed integer.
+        int intValue;
+        try {
+
+            /*
+             * Try to parse the integer. Was a negative integer received when
+             * it is not permitted?
+             */
+            intValue = Integer.parseInt(value);
+            if (!(getFlag() || (0 <= intValue))) {
+
+                /*
+                 * An impermissible negative value was received. Throw a new
+                 * CLA exception.
+                 */
+                throw new CLAException(String.format("Negative values are " +
+                                "not permitted for '%s'; %d received.",
+                        getKeyName().toLowerCase(), intValue));
+            }
+        }
+
+        // The received value cannot be parsed as an integer.
+        catch (@NotNull NumberFormatException exception) {
+            throw new CLAException(exception.getMessage());
+        }
+
+        // Set the integer value as the preference under the key name.
+        getPreferences().putInt(getKeyName(), intValue);
     }
 }
