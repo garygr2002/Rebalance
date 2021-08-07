ae48235ebfc614617dab02c8ed6a13a7dccffe69
Gary Gregg <garygr2002@yahoo.com>
2021-08-06 17:18:47 -0700

    Make the boolean interpreter check that its argument is a lowercase conversion of either 'false' or 'true'.

--- src/com/garygregg/rebalance/interpreter/BooleanInterpreter.java
+++ src/com/garygregg/rebalance/interpreter/BooleanInterpreter.java
@@ -13,6 +13,27 @@ public class BooleanInterpreter extends Interpreter<Boolean> {
 
     @Override
     protected @NotNull Boolean doInterpret(@NotNull String string) {
-        return Boolean.parseBoolean(string);
+
+        /*
+         * Convert the argument to lower case, and parse it as a boolean value.
+         * If the argument were a lowercase equivalent of 'false' or 'true',
+         * then the string conversion of the parse result should now equal the
+         * argument. If not, we have a problem.
+         */
+        final String candidate = string.toLowerCase();
+        final boolean result = Boolean.parseBoolean(candidate);
+        if (!Boolean.toString(result).equals(candidate)) {
+
+            /*
+             * The string conversion of the parse result does not equal the
+             * lowercase conversion of the argument. Throw a new illegal
+             * argument exception detailing the bad news.
+             */
+            throw new IllegalArgumentException(String.format("'%s' is " +
+                    "cannot be parsed as a boolean value", string));
+        }
+
+        // Return the boolean result.
+        return result;
     }
 }
