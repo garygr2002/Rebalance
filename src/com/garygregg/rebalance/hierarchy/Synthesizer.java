7bb2f2b93fbbf8e8b47a2ac05aeb591f9fc228a9
Gary Gregg <garygr2002@yahoo.com>
2021-09-24 17:46:49 -0700

    Add pension valuation tools for subclasses.

--- src/com/garygregg/rebalance/hierarchy/Synthesizer.java
+++ src/com/garygregg/rebalance/hierarchy/Synthesizer.java
@@ -1,17 +1,157 @@
 package com.garygregg.rebalance.hierarchy;
 
 import com.garygregg.rebalance.AccountKey;
+import com.garygregg.rebalance.InflationCaddy;
 import com.garygregg.rebalance.MessageLogger;
+import com.garygregg.rebalance.countable.Currency;
 import com.garygregg.rebalance.distinguished.DistinguishedAccountLibrary;
 import com.garygregg.rebalance.distinguished.DistinguishedAccounts;
 import org.jetbrains.annotations.NotNull;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.temporal.TemporalAdjusters;
+import java.util.Date;
 import java.util.logging.Level;
+
+import static java.time.temporal.ChronoUnit.DAYS;
+import static java.time.temporal.ChronoUnit.MONTHS;
 
 abstract class Synthesizer {
 
+    // The daily inflation getter
+    private static final Inflation daily = InflationCaddy::getDaily;
+
+    // The monthly inflation getter
+    private static final Inflation monthly = InflationCaddy::getMonthly;
+
+    // An inflation caddy instance
+    private final InflationCaddy caddy = InflationCaddy.getInstance();
+
     // Our local message logger
     private final MessageLogger messageLogger = new MessageLogger();
+
+    /**
+     * Converts a date.
+     *
+     * @param date The date to convert
+     * @return The converted date
+     */
+    private static @NotNull LocalDate convert(@NotNull Date date) {
+        return date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+    }
+
+    /**
+     * Gets the default inflation.
+     *
+     * @return The default inflation
+     */
+    private static double getDefaultInflation() {
+        return 1.;
+    }
+
+    /**
+     * Gets an inflation figure from an inflation caddy.
+     *
+     * @param inflation The figure getter
+     * @param caddy     The inflation caddy
+     * @return An inflation figure
+     */
+    private static double getInflation(@NotNull Inflation inflation,
+                                       @NotNull InflationCaddy caddy) {
+
+        /*
+         * Use the getter to access inflation from the caddy. Return a default
+         * if the result is null, otherwise return the result.
+         */
+        final Double result = inflation.getInflation(caddy);
+        return (null == result) ? getDefaultInflation() : result;
+    }
+
+    /**
+     * Calculates a pension value.
+     *
+     * @param monthly The monthly payment
+     * @param start   The start date of the calculation
+     * @param end     The end date of the calculation
+     * @param reduce  Reduce the monthly payment for inflation
+     * @return The value of the pension
+     */
+    protected @NotNull Currency calculateValue(@NotNull Currency monthly,
+                                               Date start,
+                                               @NotNull Date end,
+                                               boolean reduce) {
+
+        /*
+         * Declare the result, and initialize it to zero. Calculate the local
+         * start date.
+         */
+        Currency result = Currency.getZero();
+        final LocalDate localStart = (null == start) ? LocalDate.now() :
+                convert(start);
+
+        /*
+         * Convert the end date to a local date. Is the start date after the
+         * end date?
+         */
+        final LocalDate localEnd = convert(end);
+        if (localStart.isAfter(localEnd)) {
+
+            // The start date is after the end date. Log a warning.
+            getLogger().logMessage(Level.WARNING, String.format("Start " +
+                            "date of '%s' occurs before end date of '%s'; cannot " +
+                            "calculate value of monthly payments.",
+                    localStart, localEnd));
+        }
+
+        // The start date is not after the end date.
+        else {
+
+            /*
+             * The date of the first payment is the first day of the month
+             * after the start date.
+             */
+            final LocalDate localFirstPayment =
+                    localStart.with(TemporalAdjusters.firstDayOfNextMonth());
+
+            // Adjust the first payment for daily inflation, as requested.
+            final Currency firstPayment = reduce ? inflate(monthly, daily,
+                    DAYS.between(localStart, localFirstPayment)) : monthly;
+
+            result = geometricSum(firstPayment, reduce ? getInflation(Synthesizer.monthly, caddy) : getDefaultInflation(),
+                    MONTHS.between(localFirstPayment, localEnd));
+        }
+
+        return result;
+    }
+
+    /**
+     * Calculates a geometric sum of currency.
+     *
+     * @param currency The currency
+     * @param rate     The rate
+     * @param periods  The number of periods
+     * @return A geometric sum of the given currency
+     */
+    private @NotNull Currency geometricSum(@NotNull Currency currency,
+                                           double rate,
+                                           long periods) {
+
+        /*
+         * Declare and initialize the forbidden rate. Get the payment from the
+         * currency.
+         */
+        final double forbiddenRate = 1.;
+        final double payment = currency.getValue();
+
+        /*
+         * Convert the geometric sum, and wrap the result in a new
+         * currency object before returning it.
+         */
+        return new Currency((forbiddenRate == rate) ? payment * periods :
+                (payment - payment * Math.pow(rate, periods + 1) /
+                        (forbiddenRate - rate)));
+    }
 
     /**
      * Gets the distinguished account for which this synthesizer is intended.
@@ -37,6 +177,23 @@ abstract class Synthesizer {
      */
     protected MessageLogger getLogger() {
         return messageLogger;
+    }
+
+    /**
+     * Reduces the value of currency due to inflation.
+     *
+     * @param currency  The currency to reduce
+     * @param inflation The inflation figure getter
+     * @param periods   The number of periods of compounding (duration of period
+     *                  is defined by the figure getter)
+     * @return The value of the currency reduced due to inflation
+     */
+    @SuppressWarnings("SameParameterValue")
+    private @NotNull Currency inflate(@NotNull Currency currency,
+                                      @NotNull Inflation inflation,
+                                      long periods) {
+        return new Currency(currency.getValue() /
+                Math.pow(getInflation(inflation, caddy), periods));
     }
 
     /**
@@ -109,5 +266,16 @@ abstract class Synthesizer {
 
         // Return the result.
         return result;
+    }
+
+    private interface Inflation {
+
+        /**
+         * Gets an inflation figure from an inflation caddy.
+         *
+         * @param caddy The caddy from which to get an inflation figure
+         * @return An inflation figure
+         */
+        Double getInflation(@NotNull InflationCaddy caddy);
     }
 }
