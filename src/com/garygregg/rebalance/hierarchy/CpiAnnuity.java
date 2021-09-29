01f8a6fe875361dfc15356d381e1158ef679dd80
Gary Gregg <garygr2002@yahoo.com>
2021-09-28 17:03:17 -0700

    Add calculateValue(Currency, Date, Date).

--- src/com/garygregg/rebalance/hierarchy/CpiAnnuity.java
+++ src/com/garygregg/rebalance/hierarchy/CpiAnnuity.java
@@ -1,8 +1,10 @@
 package com.garygregg.rebalance.hierarchy;
 
+import com.garygregg.rebalance.countable.Currency;
 import com.garygregg.rebalance.distinguished.DistinguishedAccounts;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Date;
 import java.util.logging.Level;
 
 class CpiAnnuity extends AnnuitySynthesizer {
@@ -15,6 +17,20 @@ class CpiAnnuity extends AnnuitySynthesizer {
      */
     public CpiAnnuity(@NotNull DistinguishedAccounts account) {
         super(account);
+    }
+
+    /**
+     * Calculates an annuity value.
+     *
+     * @param monthly The monthly payment
+     * @param start   The start date of the calculation
+     * @param end     The end date of the calculation
+     * @return The value of the annuity
+     */
+    protected @NotNull Currency calculateValue(@NotNull Currency monthly,
+                                               Date start,
+                                               @NotNull Date end) {
+        return super.calculateValue(monthly, start, end, false);
     }
 
     @Override
