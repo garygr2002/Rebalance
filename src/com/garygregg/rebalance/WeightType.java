7f8da819fb5d54737822b72a6c65cfb65b1e1420
Gary Gregg <garygr2002@yahoo.com>
2021-11-05 19:15:22 -0700

    Add 'getMaxEntry()', 'getMaxChildren()' and a static initializer block.

--- src/com/garygregg/rebalance/WeightType.java
+++ src/com/garygregg/rebalance/WeightType.java
@@ -2,13 +2,18 @@ package com.garygregg.rebalance;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
 public enum WeightType {
 
     // No parent for 'all'.
     ALL("All"),
 
     // Tickers must contain fund type CASH.
-    CASH( "Cash general", ALL),
+    CASH("Cash general", ALL),
 
     // Tickers must contain fund type CASH and fund type TREASURY.
     CASH_GOVERNMENT("Cash government", CASH),
@@ -82,6 +87,19 @@ public enum WeightType {
     STOCK_VALUE("Stock value", STOCK_LARGE, STOCK_NOT_LARGE, STOCK_MEDIUM,
             STOCK_SMALL);
 
+    // The number of children of the weight type that has the most
+    private static final int maxChildren;
+
+    static {
+
+        /*
+         * Get a map entry containing the weight type that has the most
+         * children. Extract the type and set it as a member variable.
+         */
+        final Map.Entry<WeightType, Integer> entry = getMaxEntry();
+        maxChildren = entry.getValue();
+    }
+
     // The parents for the weight type
     private final WeightType[] parents;
 
@@ -99,6 +117,61 @@ public enum WeightType {
         // Assign the member variables.
         this.parents = parents;
         this.softName = softName;
+    }
+
+    /**
+     * Gets the number of children of the weight type that has the most.
+     *
+     * @return The number of children of the weight type that has the
+     * most
+     */
+    public static int getMaxChildren() {
+        return maxChildren;
+    }
+
+    /**
+     * Gets a map entry containing the weight type that has the most
+     * children.
+     *
+     * @return A map entry containing the weight type that has the most
+     * children
+     */
+    private static @NotNull Map.Entry<WeightType, Integer> getMaxEntry() {
+
+        /*
+         * Declare and initialize a map with weight type keys and integer
+         * values. Declare an integer to receive a count.
+         */
+        final Map<WeightType, Integer> map = new HashMap<>();
+        Integer count;
+
+        /*
+         * Declare a variable to receive an array of weight type parents. Cycle
+         * for each weight type.
+         */
+        WeightType[] parents;
+        for (WeightType type : WeightType.values()) {
+
+            // Get the parents of the first/next type. Cycle for each parent.
+            parents = type.getParents();
+            for (WeightType parent : parents) {
+
+                /*
+                 * Get the count of the first next parent from the map. Put the
+                 * parent back in the map with one if the existing count is
+                 * null. Otherwise, increment the count.
+                 */
+                count = map.get(parent);
+                map.put(parent, (null == count) ? 1 : ++count);
+            }
+        }
+
+        /*
+         * Return the map key/value entry for the weight type that has the
+         * highest count.
+         */
+        return Collections.max(map.entrySet(),
+                Comparator.comparingInt(Map.Entry::getValue));
     }
 
     /**
