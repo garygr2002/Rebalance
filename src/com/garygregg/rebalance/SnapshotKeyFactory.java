8b3bcbb6fa3f4dcd649f437fbda9b259ea8acb13
Gary Gregg <garygr2002@yahoo.com>
2021-12-17 16:59:31 -0800

    Mix up the internal key of the Snapshot key by applying a multiplicand in the SnapshotKeyFactory; hopefully still cycles through all integers.

--- src/com/garygregg/rebalance/SnapshotKeyFactory.java
+++ src/com/garygregg/rebalance/SnapshotKeyFactory.java
@@ -2,15 +2,25 @@ package com.garygregg.rebalance;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
 import java.util.Random;
 
 public class SnapshotKeyFactory {
 
-    // The largest prime less than 2^(Integer.SIZE - 1)
-    private static final long bigPrime = 2147483647L;
+    /*
+     * The two largest prime numbers that are less than or equal to
+     * 2^(Integer.SIZE - 1), or Integer.MAX_VALUE
+     */
+    private static final List<Long> bigPrimes = List.of(2147483647L, 2147483629L);
+
+    // The value to add to the current key
+    private static final long addend = bigPrimes.get(0);
 
     // A modulus for clearing bits beyond Integer.SIZE - 1
     private static final long mod = (1L << Integer.SIZE);
+
+    // The value to multiply to the sum of the current key and the addend
+    private static final long multiplicand = bigPrimes.get(1);
 
     // The current internal key
     private int key;
@@ -46,7 +56,7 @@ public class SnapshotKeyFactory {
      * @return A new snapshot key
      */
     public @NotNull SnapshotKey produce() {
-        return new SnapshotKey(key = ((int) ((Integer.toUnsignedLong(key) +
-                bigPrime) % mod)));
+        return new SnapshotKey(key = ((int) (((Integer.toUnsignedLong(key) +
+                addend) * multiplicand) % mod)));
     }
 }
