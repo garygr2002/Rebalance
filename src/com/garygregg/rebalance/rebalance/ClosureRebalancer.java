24f80de5fca738e707da1adb5839e6b0ded9cfd0
Gary Gregg <garygr2002@yahoo.com>
2021-12-22 20:35:11 -0800

    Fill out the ClosureRebalancer.

--- src/com/garygregg/rebalance/rebalance/ClosureRebalancer.java
+++ src/com/garygregg/rebalance/rebalance/ClosureRebalancer.java
@@ -1,18 +1,184 @@
 package com.garygregg.rebalance.rebalance;
 
+import com.garygregg.rebalance.BreakdownType;
+import com.garygregg.rebalance.MessageLogger;
 import com.garygregg.rebalance.WeightType;
+import com.garygregg.rebalance.countable.Currency;
 import com.garygregg.rebalance.hierarchy.Account;
+import com.garygregg.rebalance.hierarchy.Portfolio;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Map;
+import java.util.logging.Logger;
 
 class ClosureRebalancer extends WeightRebalancer {
+
+    // Zero currency
+    private static final Currency zero = Currency.getZero();
+
+    // Our message logger
+    private final MessageLogger messageLogger = new MessageLogger();
+
+    {
+
+        // Set the logger in the message logger.
+        messageLogger.setLogger(Logger.getLogger(
+                ClosureRebalancer.class.getCanonicalName()));
+    }
+
+    /**
+     * Adjusts level-zero weights in a weight map to take into account the
+     * considered value of a portfolio.
+     *
+     * @param weightMap  A weight map
+     * @param considered The considered value of a portfolio
+     */
+    private static void adjust(@NotNull Map<WeightType, Double> weightMap,
+                               double considered) {
+
+        /*
+         * Sum the existing level-zero weights in the weight map. Declare a
+         * variable to receive a weight from the map.
+         */
+        final double sum = sum(weightMap);
+        Double weight;
+        for (WeightType type : getLevelZero()) {
+
+            /*
+             * Get the existing weight for the first/next level-zero type.
+             * Replace (or add, if necessary) the mapping for the type by
+             * multiplying the existing weight by the considered value of the
+             * portfolio, and dividing by the calculated weight sum.
+             */
+            weight = weightMap.get(type);
+            weightMap.put(type, ((null == weight) ? 0. : weight) *
+                    considered / sum);
+        }
+    }
+
+    /**
+     * Sums the level-zero weights in a weight map.
+     *
+     * @param weightMap The weight map
+     * @return The sum of the level-zero weights in the map
+     */
+    private static double sum(@NotNull Map<WeightType, Double> weightMap) {
+
+        /*
+         * Declare and initialize the sum. Declare a variable to receive a
+         * weight. Cycle for each level-zero weight type.
+         */
+        double sum = 0.;
+        Double weight;
+        for (WeightType type : getLevelZero()) {
+
+            // Get the weight for the first/next type. Is the weight not null?
+            weight = weightMap.get(type);
+            if (null != weight) {
+
+                // The weight is not null. Add it to the sum.
+                sum += weight;
+            }
+        }
+
+        // Return the sum.
+        return sum;
+    }
+
+    /**
+     * Gets the message logger for the closure rebalancer.
+     *
+     * @return The message logger for the closure rebalancer
+     */
+    private MessageLogger getLogger() {
+        return messageLogger;
+    }
 
     @Override
     protected @NotNull Map<WeightType, Double> getWeights(@NotNull Account account,
                                                           boolean adjust) {
 
-        // TODO: Change this to use getWeightsForClosure(Account, boolean).
-        return super.getWeights(account, adjust);
+        // Get the weights-for-closure.
+        final Map<WeightType, Double> weightMap = getWeightsForClosure(account,
+                adjust);
+
+        // Get the current portfolio and its value.
+        final Portfolio portfolio = getCurrent();
+        final Currency portfolioValue = (null == portfolio) ? zero :
+                portfolio.getConsidered();
+
+        /*
+         * Adjust the portfolio map with the portfolio value. Is the current
+         * portfolio not null?
+         */
+        adjust(weightMap, portfolioValue.getValue());
+        if (null != portfolio) {
+
+            /*
+             * The current portfolio is not null. Break down the proposed
+             * values of the portfolio, and subtract existing proposed values
+             * from desired values for the level zero weight types.
+             */
+            portfolio.breakdown(BreakdownType.PROPOSED);
+            subtract(weightMap, portfolio);
+        }
+
+        // Return the modified weight map.
+        return weightMap;
+    }
+
+    /**
+     * Subtracts existing values in a portfolio from desired values for level-
+     * zero weight types.
+     *
+     * @param weightMap The weight map to modify
+     * @param portfolio A portfolio
+     */
+    private void subtract(@NotNull Map<WeightType, Double> weightMap,
+                          @NotNull Portfolio portfolio) {
+
+        /*
+         * Declare desired and existing weights. Cycle for each level zero
+         * weight type.
+         */
+        double desired, existing;
+        for (WeightType type : getLevelZero()) {
+
+            /*
+             * Get the desired and existing weights. Is the existing value less
+             * than the desired value?
+             */
+            desired = weightMap.get(type);
+            existing = portfolio.getProposed(type).getValue();
+            if (existing < desired) {
+
+                /*
+                 * The existing value is less than the desired value. Subtract
+                 * the existing value from the desired value, and replace the
+                 * value in the weight map.
+                 */
+                weightMap.put(type, desired - existing);
+            }
+
+            // The existing value is *not* less than the desired value.
+            else {
+
+                /*
+                 * Log and stream a message if the desired value is less than
+                 * the existing value.
+                 */
+                if (desired < existing) {
+                    getLogger().streamAndLog(MessageLogger.getExtraordinary(),
+                            String.format("At rebalance closure, portfolio " +
+                                            "'%s' already %s for weight type %s; " +
+                                            "desired value is %s.", portfolio.getKey(),
+                                    type, Currency.format(existing),
+                                    Currency.format(desired)));
+                }
+
+                // Replace the existing value in the weight map with zero.
+                weightMap.put(type, zero.getValue());
+            }
+        }
     }
 }
