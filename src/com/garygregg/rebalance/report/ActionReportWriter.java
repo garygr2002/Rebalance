5ad06ab0eed31dcd2a7a04b73d048a9949606676
Gary Gregg <garygr2002@yahoo.com>
2022-01-03 18:23:42 -0800

    Fill out more of the ActionReportWriter.

--- src/com/garygregg/rebalance/report/ActionReportWriter.java
+++ src/com/garygregg/rebalance/report/ActionReportWriter.java
@@ -1,26 +1,126 @@
 package com.garygregg.rebalance.report;
 
+import com.garygregg.rebalance.AccountKey;
+import com.garygregg.rebalance.Pair;
+import com.garygregg.rebalance.account.AccountDescription;
+import com.garygregg.rebalance.countable.Currency;
+import com.garygregg.rebalance.countable.MutableCurrency;
 import com.garygregg.rebalance.hierarchy.Account;
 import com.garygregg.rebalance.hierarchy.Institution;
 import com.garygregg.rebalance.hierarchy.Portfolio;
 import com.garygregg.rebalance.hierarchy.Ticker;
+import com.garygregg.rebalance.portfolio.PortfolioDescription;
+import com.garygregg.rebalance.ticker.TickerDescription;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 public class ActionReportWriter extends HierarchyWriter {
 
-    // The common (but temporary) format string
-    private static final String format = "Writing about %s '%s'.\n";
+    // A string to separate accounts in a report
+    private static final String accountSeparator =
+            String.format("%s\n", "-".repeat(65));
+
+    // The common portfolio/institution/account declaration string
+    private static final String format = "Required rebalance actions for " +
+            "%s key '%s'.\n";
+
+    // A string to separate institutions in a report
+    private static final String institutionSeparator =
+            String.format("%s\n", "*".repeat(80));
+
+    // The value of zero currency
+    private static final Currency zero = Currency.getZero();
+
+    // A reusable list of currency/ticker pairs
+    final List<Pair<MutableCurrency, Ticker>> differencePairs =
+            new ArrayList<>();
+
+    // A replacement action for building the currency/ticker pair list
+    private final Action<Ticker> tickerAction = (writer, ticker) -> {
+
+        /*
+         * Get the description from the ticker. Is the description not null,
+         * and does the non-null description indicate that the ticker is
+         * considered for rebalance?
+         */
+        final TickerDescription description = ticker.getDescription();
+        if ((null != description) && description.isConsidered()) {
+
+            /*
+             * The ticker description is not null, and the non-null description
+             * indicates that the ticker is considered for rebalance. Add a
+             * difference pair for the ticker.
+             */
+            addDifferencePair(ticker);
+        }
+    };
+
+    /**
+     * Writes a name string.
+     *
+     * @param writer The file writer to receive the report lines
+     * @param name   The name to write
+     * @throws IOException Indicates an I/O exception occurred
+     */
+    private static void writeName(@NotNull FileWriter writer, String name)
+            throws IOException {
+        writer.write(String.format("Name: '%s'\n", (null == name) ?
+                "<name unavailable>" : name));
+    }
+
+    /**
+     * Adds a difference pair.
+     *
+     * @param ticker The ticker for which to add a difference pair
+     */
+    private void addDifferencePair(@NotNull Ticker ticker) {
+
+        // Get the considered and proposed values of the ticker.
+        final Currency considered = ticker.getConsidered();
+        final Currency proposed = ticker.getProposed();
+
+        /*
+         * Create a new mutable currency with the proposed value of the ticker
+         * if the proposed value is not null. If it is null, use zero. Is the
+         * considered value of the ticker not null?
+         */
+        final MutableCurrency currency =
+                new MutableCurrency((null == proposed) ? zero : proposed);
+        if (null != considered) {
+
+            /*
+             * The considered value of the ticker is not null. Subtract it from
+             * the proposed value.
+             */
+            currency.subtract(considered);
+        }
+
+        /*
+         * Add a new difference pair with the value just calculated and the
+         * ticker, but only if the difference value is not zero.
+         */
+        if (currency.isNotZero()) {
+            differencePairs.add(new Pair<>(currency, ticker));
+        }
+    }
 
     @Override
     protected void doPreCycle(@NotNull FileWriter writer,
                               @NotNull Portfolio portfolio)
             throws IOException {
 
-        // TODO: Write about the portfolio.
+        // Write the portfolio key. Get the portfolio description.
         writer.write(String.format(format, "portfolio", portfolio.getKey()));
+        final PortfolioDescription description = portfolio.getDescription();
+
+        // Write the portfolio name.
+        writeName(writer, (null == description) ? "<name unavailable>" :
+                description.getName());
     }
 
     @Override
@@ -28,9 +128,11 @@ public class ActionReportWriter extends HierarchyWriter {
                               @NotNull Institution institution)
             throws IOException {
 
-        // TODO: Write about the institution.
+        // Write an institution separator before describing the institution.
+        writer.write(institutionSeparator);
         writer.write(String.format(format, "institution",
                 institution.getKey()));
+        writeName(writer, institution.getName());
     }
 
     @Override
@@ -38,8 +140,15 @@ public class ActionReportWriter extends HierarchyWriter {
                               @NotNull Account account)
             throws IOException {
 
-        // TODO: Write about the account.
-        writer.write(String.format(format, "account", account.getKey()));
+        // Write the account separator followed by the account key.
+        writer.write(accountSeparator);
+        writer.write(String.format(format, "account",
+                AccountKey.format(account.getKey().getSecond())));
+
+        // Write the name of the account.
+        final AccountDescription description = account.getDescription();
+        writeName(writer, (null == description) ? "<name unavailable>" :
+                description.getName());
     }
 
     @Override
@@ -47,14 +156,17 @@ public class ActionReportWriter extends HierarchyWriter {
         return "action";
     }
 
-    @Override
-    protected void writeLines(@NotNull FileWriter writer,
-                              @NotNull Institution institution)
-            throws IOException {
+    /**
+     * Publishes rebalances actions contained in the differences list.
+     *
+     * @param writer The file writer to receive the report lines
+     * @throws IOException Indicates an I/O exception occurred
+     */
+    private void publishActions(@NotNull FileWriter writer) throws IOException {
 
-        // Write two newlines then call the superclass method.
-        writer.write("\n\n");
-        super.writeLines(writer, institution);
+        // TODO: Fill this out.
+        writer.write("Rebalance actions are required for this " +
+                "account!\n");
     }
 
     @Override
@@ -62,9 +174,47 @@ public class ActionReportWriter extends HierarchyWriter {
                               @NotNull Account account)
             throws IOException {
 
-        // Write a newline then call the superclass method.
+        /*
+         * Write a newline before performing the pre-cycle action. Clear the
+         * difference pairs list.
+         */
         writer.write("\n");
-        super.writeLines(writer, account);
+        doPreCycle(writer, account);
+        differencePairs.clear();
+
+        /*
+         * Iterate over the children of the account using the ticker action.
+         * Sort the difference pairs by their difference components.
+         */
+        iterate(writer, account.getChildren(), tickerAction);
+        differencePairs.sort(Comparator.comparing(Pair::getFirst));
+
+        /*
+         * Publish the required rebalance actions if there are one or more
+         * difference pairs for the account.
+         */
+        if (0 < differencePairs.size()) {
+            publishActions(writer);
+        }
+
+        /*
+         * Otherwise, write a message describing that no rebalance actions are
+         * required.
+         */
+        else {
+            writer.write("No rebalance actions are required for this " +
+                    "account.\n");
+        }
+    }
+
+    @Override
+    protected void writeLines(@NotNull FileWriter writer,
+                              @NotNull Institution institution)
+            throws IOException {
+
+        // Write a newline, then call the superclass method.
+        writer.write("\n");
+        super.writeLines(writer, institution);
     }
 
     @Override
@@ -72,7 +222,7 @@ public class ActionReportWriter extends HierarchyWriter {
                               @NotNull Ticker ticker)
             throws IOException {
 
-        // TODO: Write about the ticker.
+        // TODO: Delete this.
         writer.write(String.format(format, "ticker", ticker.getKey()));
     }
 }
