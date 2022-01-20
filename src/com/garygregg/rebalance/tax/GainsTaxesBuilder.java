63b5b017536ee6b832a0521b5c5793e03c37477a
Gary Gregg <garygr2002@yahoo.com>
2022-01-19 19:02:49 -0800

    Remove  unused private method 'getGainsTaxLibrary().'

--- src/com/garygregg/rebalance/tax/GainsTaxesBuilder.java
+++ src/com/garygregg/rebalance/tax/GainsTaxesBuilder.java
@@ -1,43 +1,10 @@
 package com.garygregg.rebalance.tax;
 
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 abstract class GainsTaxesBuilder extends TaxesBuilder {
-
-    /**
-     * Gets the capital gains tax library.
-     *
-     * @return The capital gains tax library
-     */
-    private @Nullable GainsTaxLibrary getGainsTaxLibrary() {
-
-        /*
-         * Get the tax library. Cast the tax library to a capital-gains tax
-         * library if it is an instance of such. Otherwise, use null. Is the
-         * capital-gains library null?
-         */
-        final TaxLibrary library = getLibrary();
-        final GainsTaxLibrary gainsTaxLibrary =
-                (library instanceof GainsTaxLibrary) ?
-                        ((GainsTaxLibrary) library) : null;
-        if (null == gainsTaxLibrary) {
-
-            /*
-             * The capital gains tax library is null. This is an error. Log it
-             * as such.
-             */
-            logMessage(Level.SEVERE, String.format("The builder for " +
-                    "the '%s' capital gains tax library is not associated " +
-                    "with a capital gains tax library!", getPrefix()));
-        }
-
-        // Return the capital gains tax library.
-        return gainsTaxLibrary;
-    }
 
     @Override
     public @NotNull String getPrefix() {
@@ -48,5 +15,4 @@ abstract class GainsTaxesBuilder extends TaxesBuilder {
     protected @NotNull Logger getReadingLogger() {
         return Logger.getLogger(GainsTaxesBuilder.class.getCanonicalName());
     }
-
 }
