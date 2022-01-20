7469d39c018f49068d683e4be48d1cd01e486192
Gary Gregg <garygr2002@yahoo.com>
2022-01-19 19:05:15 -0800

    Remove the unused 'qualifier' and 'query' packages and their classes (actually moved to non-version controlled location).

--- src/com/garygregg/rebalance/qualifier/AndConjunction.java
+++
@@ -1,9 +0,0 @@
-package com.garygregg.rebalance.qualifier;
-
-public class AndConjunction<T> extends Conjunction<T> {
-
-    /**
-     * Constructs the 'and' conjunction.
-     */
-    public AndConjunction() { super(false); }
-}
